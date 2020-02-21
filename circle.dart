1568ae5dbf2f1a317a19532f6ca9012874115a6a
itachi-P <itachip38@gmail.com>
2020-02-21 21:45:02 +0900

    Modified circle.dart

--- circle.dart
+++ circle.dart
@@ -11,19 +11,11 @@ class Circle {
 }
 
 void main() {
-  // Before Dart 2.1, you had to provide a trailing `.0` – `42.0` – when
-  // assigning to fields or parameters of type `double`.
-  // A value like `42` was not allowed.
 
   print(Circle(2)); // Display "Instance of 'Circle'"
   
   print(Circle(5).diameter);
-  
   print(Circle(4.0).area); // Before Dart 2.1, the trailing `.0` is required.
-
-  /* With Dart 2.1, you can provide whole-number values when assigning to
-   * a double without the trailing `.0`.
-  */
   print(Circle(4).circumference); // Legal with Dart 2.1
 }
 
