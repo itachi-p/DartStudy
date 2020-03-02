3332d34ac1e252ffb14a5cbd6c8fbe04be7b9ac2
itachi-P <itachip38@gmail.com>
2020-03-02 23:42:57 +0900

    Studing type of Built-in.

--- lesson_01.dart
+++ lesson_01.dart
@@ -5,8 +5,16 @@ printInteger(int aNumber) {
 
 // This is where the app starts executing.
 main() {
-  var number01 = 5; // Declare and initialize a variable.
-  num number02 = number01 + 3.14159; // 'int' & 'double' are subtype or 'num'
+  var number01 = 5; // Declare and initialize a variable.('var' or 'int')
+
+  String piAsString = 3.14159.toStringAsFixed(2); // double -> String
+  assert(piAsString == '3.14');
+
+  num number02 = num.parse(piAsString); // 'int' & 'double' are subtype or 'num'
+  print(number02);
+  number02 += number01; // int + double -> double
+  print(number02);
+
   var number03 = int.parse('13');
   assert(number03 == 13);
 
