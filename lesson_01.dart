8fc70b9a1343ae13213a7ea4b3ed1add425bd215
itachi-P <itachip38@gmail.com>
2020-03-02 23:06:15 +0900

    Studing type of Built-in of Dart language.

--- lesson_01.dart
+++ lesson_01.dart
@@ -5,6 +5,10 @@ printInteger(int aNumber) {
 
 // This is where the app starts executing.
 main() {
-  var number = 42; // Declare and initialize a variable.
-  printInteger(number); // Call a function.
+  var number01 = 5; // Declare and initialize a variable.
+  num number02 = number01 + 3.14159; // 'int' & 'double' are subtype or 'num'
+  var number03 = int.parse('13');
+  assert(number03 == 13);
+
+  printInteger(number02.toInt() + number03); // Call a function.
 }
