7e602233c5a90e9d2c7a454e29d18f07d22d23b2
itachi-P <itachip38@gmail.com>
2020-03-03 09:26:48 +0900

    Studing type of Built-in (List) part2.

---
+++ lesson_02_List.dart
@@ -0,0 +1,27 @@
+main() {
+  var list1 = <int>[];
+  // The following notation has the same meaning.
+  //var list2 = List<int>();
+  //List<int> list3 = [];
+
+  list1.add(100);
+  list1.add(200);
+  list1.add(100 + list1[1]);
+  print(list1);
+
+  list1[1] = 101;
+  list1[2] = list1[1] + 100;
+  list1.add(list1[2] + 100);
+  print(list1);
+  assert(list1.elementAt(1) == 101);
+
+  // The following is a runtime error:
+  // RangeError (index): Invalid value: Not in range 0..3, inclusive: 4
+  //print(list[4]);
+  //list[4] = 400;
+
+  var list4 = Iterable<int>.generate(3).toList();
+  print("\n list4 : $list4");
+  var list5 = List<String>.generate(10, (i) => "num" + i.toString());
+  print('\n list5 : $list5');
+}
