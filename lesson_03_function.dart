4574e3270c6f3605e7a4a1275801276fd6808590
itachi-P <itachip38@gmail.com>
2020-03-03 22:16:17 +0900

    Added a function to display Fibonacci sequence.

--- lesson_03_function.dart
+++ lesson_03_function.dart
@@ -12,8 +12,23 @@ double multiply(double a, double b) {
   return a * b;
 }
 
+List fibonacci(int num) {
+  int f0 = 0;
+  int f1 = 1;
+  int f2;
+  List fibo = [f0, f1];
+  for (int i = 0; i < num; i++) {
+    f2 = f0 + f1;
+    f0 = f1;
+    f1 = f2;
+    fibo.add(f1);
+  }
+  return fibo;
+}
+
 main() {
   printPi();
   print(subtract(3, 5));
   print(multiply(8, 9));
+  print(fibonacci(10));
 }
