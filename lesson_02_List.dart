2e9364e18a679c1c46dc3221bdc4f667a144e794
itachi-P <itachip38@gmail.com>
2020-03-03 17:56:33 +0900

    Studing type of Built-in (List, Set, Map).

--- lesson_02_List.dart
+++ lesson_02_List.dart
@@ -1,8 +1,10 @@
 main() {
+  // List
+
   var list1 = <int>[];
   // The following notation has the same meaning.
-  //var list2 = List<int>();
-  //List<int> list3 = [];
+  var list2 = List<int>();
+  List<int> list3 = [];
 
   list1.add(100);
   list1.add(200);
@@ -25,4 +27,29 @@ main() {
 
   var list5 = List<String>.generate(10, (i) => "num" + i.toString());
   print('\n list5 : $list5');
+
+  // Set  Unordered collection of unique values
+
+  Set set1 = {};
+  set1.add(1000);
+  Set set2 = const {};
+  // Attempting to assign to a const variable results in a runtime error.
+  //set2.add(200);
+
+  Set<String> set3 = {};
+  var set4 = <String>[];
+
+  // Map  The object that associates the key with the value
+
+  var map1 = Map<String, int>();
+  map1['foo'] = 100;
+  map1['bar'] = 200;
+  print("\n map1 $map1");
+
+  var map2 = {
+    // Key:     Value
+    "firstKey": "firstValue"
+  }; // In this notation, it is a Map, not a Set.
+  map2['secondKey'] = 'secondValue';
+  print("\n map2 $map2");
 }
