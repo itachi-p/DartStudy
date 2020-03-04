53c7fc23f588250bf0a5d4ef7e8f4d911b72e2b9
itachi-P <itachip38@gmail.com>
2020-03-04 12:05:04 +0900

    Learn Named parameters and Closure.

--- lesson_03_function.dart
+++ lesson_03_function.dart
@@ -33,7 +33,11 @@ List fibonacci(int num) {
 
 // {}で囲った部分の名前付きパラメーターは呼び出し側で名前指定する。
 // 同時に引数は任意指定になるが、必須にしたい場合@requiredアノテーション(meta.dartのimportが必要)を付ける。
-void enableFlags(bool bold, {bool italic, @required bool hidden}) {}
+// 名前付きパラメーター、及びオプショナルパラメーターには初期値(コンパイル時定数)を設定できる。
+// bool型でデフォルト値が設定されていない場合、trueでもfalseでもなくnullになる。
+void enableFlags(bool bold, {bool italic = false, @required bool hidden}) {}
+
+void aaaBBB(bool a, [bool optionalParameter = false]) {}
 
 main() {
   printPi();
@@ -43,6 +47,34 @@ main() {
 
   enableFlags(true, italic: true, hidden: false);
   enableFlags(true, hidden: true);
-  enableFlags(true, italic: false); // error(note) "hidden" required
+  //enableFlags(true, italic: false); // error(note) "hidden" required
   //enableFlags(hidden: null) // error  1st argument "bold" required
+
+  // Anonymous function
+// Dartは関数もオブジェクトとして変数に代入したり、引数として他の関数に渡すことができる。
+// 関数はFunction型になる。
+      (int num) {
+    return true;
+  };
+  // for example.
+  var hey = (name) {
+    return 'Hey, $name!';
+  };
+  print(hey('Tarou'));
+
+  // When specifying return type without relying on type inference
+  String Function(String name) hello = (name) {
+    return 'Hello, $name!!';
+  };
+  print(hello('Hanako'));
+
+  // Closure
+  Function makeAdder(num addBy) {
+    return (num i) => addBy + i;
+  }
+
+  var add2 = makeAdder(2);
+  var add4 = makeAdder(4);
+  assert(add2(3) == 5);
+  assert(add4(3) == 7);
 }
